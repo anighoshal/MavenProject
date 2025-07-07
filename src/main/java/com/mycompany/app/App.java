e6d19fa4f25d3987b73492356ca4b4e45a00b484
anighoshal <anirbanghoshal990@gmail.com>
2025-07-08 01:12:49 +0530

    New code added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
-	System.out.println("Hello Jenkins CLI");
+	System.out.println("Hello New World!");
     }
 
     public String getMessage() {
